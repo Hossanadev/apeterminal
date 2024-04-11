4cf02aaa19d4507f02f2bcbda9912f70cb362d5f
Hossanadev <hossanacc@gmail.com>
2024-04-11 14:59:25 +0100

    removed unecessary use-client tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-"use client"
-
 import {TopBar} from "@/components/TopBar";
 import {Hero} from "@/components/Hero";
 import {Body} from "@/components/Body";
 import {Footer} from "@/components/Footer";
-import {useState} from "react";
 
 export default function Home() {
   return (
