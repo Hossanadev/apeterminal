01c2adfb9916fe32acc2fc21695eeaf996b43ff9
TEMHITHORPHE <tehmhithorphe@gmail.com>
2024-04-17 18:17:14 +0100

    Wagmi provider wrap app context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Web3ModalProvider from "./providers/Web3ModalProvider";
+import { cookieToInitialState } from "wagmi";
+import { config } from "./Web3ModalConfig";
+import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +18,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const initialState = cookieToInitialState(config, headers().get('cookie'))
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Web3ModalProvider initialState={initialState}>
+          {children}
+        </Web3ModalProvider>
+      </body>
     </html>
   );
 }
