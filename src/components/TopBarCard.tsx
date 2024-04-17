37832d451cae1adb67bd7aafb619e0af5ee4211c
TEMHITHORPHE <tehmhithorphe@gmail.com>
2024-04-17 18:16:20 +0100

    Added: OnCLick button props

--- src/components/TopBarCard.tsx
+++ src/components/TopBarCard.tsx
@@ -10,19 +10,22 @@ type TopBarCardType = {
     icon: string;
     hideIcon?: boolean;
     borderRadius?: string;
-    backgroundColor?: string
-    className?: string
+    backgroundColor?: string;
+    className?: string;
+    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
 }
 
-export const TopBarCard: React.FC<TopBarCardType> = ({icon, name, borderColor,
-                                                         textColor, backgroundColor, borderRadius, linkURL, className, hideIcon}) => {
+export const TopBarCard: React.FC<TopBarCardType> = ({ onClick, icon, name, borderColor,
+    textColor, backgroundColor, borderRadius, linkURL, className, hideIcon }) => {
     return (
-        <Link href={linkURL}>
-                <div style={{borderColor: borderColor, backgroundColor: backgroundColor, borderRadius: borderRadius}} className={`flex gap-x-2 py-2.5 px-5 border-[0.5px] rounded-xl font-medium` +
-            className}>
-                <Image src={icon} alt={name} className={`${hideIcon ? "hidden" : ""} inline-block h-[20px] w-[20px]`}/>
-                <p style={{color: textColor}} className={"inline-block text-[14px] align-middle mx-auto"}>{name}</p>
-                </div>
+        <Link href={linkURL} onClick={onClick}>
+            <div style={{ borderColor: borderColor, backgroundColor: backgroundColor, borderRadius: borderRadius }} className={`flex gap-x-2 py-2.5 px-5 border-[0.5px] rounded-xl font-medium` +
+                className}>
+                <Image src={icon} alt={name} className={`${hideIcon ? "hidden" : ""} inline-block h-[20px] w-[20px]`} />
+                <p style={{ color: textColor }} className={"inline-block text-[14px] align-middle mx-auto"}>
+                    {name}
+                </p>
+            </div>
         </Link>
     )
 }
